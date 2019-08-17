548ca41bb6a85d26076dfda2e99d8d68dd18caea
yosukesan <y.otsuki30@gmail.com>
2019-08-17 14:35:24 +0900

    modified for appveyor for testing

--- graph/bfs/run_ci.py
+++ graph/bfs/run_ci.py
@@ -4,14 +4,21 @@ import os
 class CI:
 
     def __init__(self):
+        import platform
+
         self._dir_build = os.path.join(os.getcwd(),"build_dir")
-        self._dir_install = "bfs-0.0.0"
+        self._dir_install = "ci-bfs"
+
+        self._os = ""
+        if platform.system() == "Windows":
+            self._os = " -G \"NMake Makefiles\" "
 
     def __configure__(self):
         os.mkdir(self._dir_build)
         os.chdir(self._dir_build)
 
-        cmd_configure = "cmake " + os.path.join("..") + " -DCMAKE_INSTALL_PREFIX=" + os.path.join("..", self._dir_install)
+        cmd_configure = "cmake " + self._os + os.path.join("..") + " -DCMAKE_INSTALL_PREFIX=" + os.path.join("..", self._dir_install)
+        print ("\n\n\n CONFIGURE: " + cmd_configure)
         os.system(cmd_configure)
 
     def __build__(self):
@@ -38,5 +45,3 @@ class CI:
 if __name__ == "__main__":
     ci = CI()
     ci.run()
-
-
